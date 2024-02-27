f011fef7c0d881876960f3ff6f7e23700f12da64
Jonathan Perry <jpper@umich.edu>
2024-02-26 18:42:46 -0600

    Add working app with several packages installed

---
+++ constants/Colors.ts
@@ -0,0 +1,14 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  primary: '#1063FD',
+  muted: '#3A5A92',
+  background: '#EFEEF6',
+  gray: '#6E6E73',
+  lightGray: '#DCDCE2',
+  green: '#4FEE57',
+  lightGreen: '#DBFFCB',
+  red: '#EF0827',
+  yellow: '#FCC70B',
+};
